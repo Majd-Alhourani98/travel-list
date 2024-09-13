128bd1c1418ce47c9e0366516c8e5469b079b40e
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-09-13 23:25:08 +0300

    rendering list items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
+
 function App() {
   return (
     <div className="app">
@@ -16,13 +22,38 @@ function Logo() {
 function Form() {
   return (
     <div className="add-form">
-      <h3>What did you need for your ~`😍 trip?</h3>
+      <h3>What did you need for your 😍 trip?</h3>
     </div>
   );
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span
+        style={
+          item.packed
+            ? { textDecoration: "line-through" }
+            : { textDecoration: "none" }
+        }
+      >
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
