718c509f241a5c5220aff112afc33a3414ff4be5
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-11-03 18:19:53 +0300

    feat: add form inputs and static list rendering for packing items

--- src/App.js
+++ src/App.js
@@ -43,9 +43,24 @@ function Logo() {
 // Displays a form for users to add new items to the packing list.
 // (Interactivity will be added later.)
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Form submitted!");
+  }
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
+
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
     </form>
   );
 }
