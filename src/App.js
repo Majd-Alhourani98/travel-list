96d19ba0b397fe2452e8db7ed8ad3ea1a95df665
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-11-04 00:54:10 +0300

    feat: implement item toggle and sorting functionality

--- src/App.js
+++ src/App.js
@@ -120,10 +120,25 @@ function Form({ onAddItem }) {
 // Currently renders a static array `initialItems`.
 // Each item is passed down to the <Item /> component.
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = [...items];
+
+  if (sortBy === "description")
+    sortedItems = [...items].sort((a, b) =>
+      a.description.localeCompare(b.description)
+    );
+
+  if (sortBy === "packed")
+    sortedItems = [...items].sort(
+      (a, b) => Number(a.packed) - Number(b.packed)
+    );
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -132,6 +147,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
